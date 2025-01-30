b3327708071b1fa64fb61669223f811cb7c28c75
Frnak <fddewaard@gmail.com>
2025-01-30 20:19:17 +0100

    5: smallest multiple (o.b.v. greatest common divisor en lowest common multiple)

---
+++ 5/smallest_multiple_with_gcd.py
@@ -0,0 +1,19 @@
+def get_gcd(num1, num2):
+    if num2 == 0:
+        return num1
+    else:
+        return get_gcd(num2, num1 % num2)
+
+def get_lcm(num1, num2):
+    return num2 * (num1 / get_gcd(num1, num2))
+
+def get_lcm_of_range(max):
+    result = max
+    for i in range(1, max):
+        result = get_lcm(i, result)
+    return result
+
+def run(num):
+    print(int(get_lcm_of_range(num)))
+
+run(20)
