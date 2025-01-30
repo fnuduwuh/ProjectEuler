b9e7d1a5204cf295815cf9ef2b85ee93f0aa6808
Frnak <fddewaard@gmail.com>
2025-01-30 19:18:30 +0100

    4: palindrome numbers

--- 4/palindrome.py
+++ 4/palindrome.py
@@ -1,9 +1,22 @@
-def palindrome(max_number):
-    num = max_number
+# input is maximale nummer (dus voor 3-digit numbers zoals opgave is de max 999)
+# palindrome checker loopt de hoogste honder nummers af en retourneert het eerste palindroom
+# als deze hoger is dan de huidige max_palindrome wordt deze overschreven
+
+def run_palindrome_checker(max_number):
+    max_palindrome = 0
+    number_to_check = max_number
+    while max_number - number_to_check < 100: # check alleen de hoogste 100 t.b.v. performance
+        uitkomst: int = palindrome(number_to_check)
+        if uitkomst > max_palindrome:
+            max_palindrome = uitkomst
+        number_to_check -= 1
+    print(max_palindrome)
+
+def palindrome(number_to_check):
+    num = number_to_check
     for i in range(num):
-        if is_palindrome(num*max_number):
-            print(str(num) + ' keer ' + str(max_number) + ' is ' + str(num * max_number))
-            return num*max_number
+        if is_palindrome(num * number_to_check):
+            return num*number_to_check
         else:
             num -= 1
     return 0
@@ -16,15 +29,5 @@ def is_palindrome(number):
         if num1 != num2:
             return False
     return True
-
-def run_palindrome_checker(number):
-    max_palindrome = 0
-    number_to_check = number
-    while number - number_to_check < 100: # check alleen de hoogste 100 t.b.v. performance
-        uitkomst: int = palindrome(number_to_check)
-        if uitkomst > max_palindrome:
-            max_palindrome = uitkomst
-        number_to_check -= 1
-    print(max_palindrome)
 
 run_palindrome_checker(999)
